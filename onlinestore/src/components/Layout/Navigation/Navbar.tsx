5a7d4cd151609cd257734bde5ef359c0fae2561d
kiokomutunga <kiokomutungapp@gmail.com>
2025-11-18 19:30:19 +0300

    navigation dropdown

--- onlinestore/src/components/Layout/Navigation/Navbar.tsx
+++ onlinestore/src/components/Layout/Navigation/Navbar.tsx
@@ -1,5 +1,35 @@
 import { Link } from "react-router-dom";
+
 function Navbar (){
+
+const navigation = [
+  {name: 'Home' , href: '/'},
+  {name: 'Shop', href: '/shop',
+  dropdown: [
+    {name: 'All Products', href: '/shop/all'},
+    {name: 'New Arrivals', href: '/shop/new'},
+
+  ]
+  },
+{
+  name: 'categories',
+  href: '/categories',
+  dropdown: [
+    {name:'Tables', href: '/category/tables' },
+    {name: 'Bedroom', href:'/category/bedroom'},
+    {name: 'Office', href: '/category/office'},
+    {name: 'Storage', href: '/category/storage'},
+    
+  ]
+
+},
+
+{name: 'Offers', href:'/offers'},
+
+{name: 'Contact', href: '/contact'},
+
+]
+
 
   return(
     <header>
@@ -17,6 +47,44 @@ function Navbar (){
               Custom <span>Kicks</span>
             </span>
           </Link>
+
+          <nav>
+            {
+              navigation.map((item) =>(
+                <div key={item.name}>
+
+                  {item.dropdown?(
+                    <>
+                    <Link to={item.href}
+                    >
+                      {item.name}
+                    </Link>
+
+                    <div>
+                      {item.dropdown.map((subItem) =>(
+                        <Link
+                        key={subItem.name}
+                        to={subItem.href}>
+
+                          {subItem.name}
+                        </Link>
+                      ))}
+                    </div>
+                    
+                    </>
+
+                  ):(
+                    <Link to={item.href}>
+                      {item.name}
+                    </Link>
+                  )}
+
+                </div>
+              ))
+            }
+          </nav>
+
+
         </div>
       </div>
 
